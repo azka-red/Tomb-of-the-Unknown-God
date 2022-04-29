7af632d3c137ab5ee97da819e3a9d2cf3bb5dfc6
Azka-Red <vegetasc2gamer@gmail.com>
2022-04-29 17:10:11 -0400

    added angle based movement to player and angle display

--- main.py
+++ main.py
@@ -1,51 +1,74 @@
 import pygame
 import sys
+import math
 from settings import SETTINGS, COLOR
 from maps import TEST_ROOM
 
+
 class Player:
-    def __init__(self, px: float, py: float, size, speed: float) -> None:
+    def __init__(self, px: float, py: float, size, speed: float, pa: float,ts:float) -> None:
         # player coordinates
         self.px = px
         self.py = py
+        self.pa = math.radians(pa/math.pi)
+        self.da=0
         self.size = size
         self.speed = speed
-        self.velocity = [0, 0]
+        self.ts=ts
+        self.movement = 0 # 0 is in place, 1 is moving forward, -1 is moving backwards
 
+    def update(self, dt):
+        self.px += self.movement*(self.speed*dt)*math.cos(self.pa)
+        self.py += self.movement*(self.speed*dt)*math.sin(self.pa)
 
-    def update(self,dt):
-        self.px += self.velocity[0]*(self.speed*dt)
-        self.py += self.velocity[1]*(self.speed*dt)
+        self.pa+=self.da*(self.ts*dt)
+        if self.pa>2*math.pi:
+            self.pa=-2*math.pi
+        
+        elif self.pa>2*math.pi:
+            self.pa=-2*math.pi
+        
+        
 
 # update stuff according to delta time value
+
+
 def update(dt):
     player.update(dt)
 
-def draw_2d_map():
-    tz=SETTINGS["TILE_SIZE"]
-    for y in range (0,len(TEST_ROOM)):
-        for x in range (0,len(TEST_ROOM[0])):
-            xo=x*tz
-            yo=y*tz
-            if TEST_ROOM[y][x]==1:
-                _color=COLOR["WHITE"]
-            else:
-                _color=COLOR["BLACK"]
-            pygame.draw.rect(surface=surface, color=_color,
-                                rect=pygame.Rect(xo,yo,tz-1,tz-1))
-                                   
-                
 
-def draw():
-    # draw background
-    surface.fill(COLOR["DARK_GRAY"])
-    # draw map
-    draw_2d_map()
-    # Draw Player as a square    
+def draw_2d_map():
+    tz = SETTINGS["TILE_SIZE"]
+    for y in range(0, len(TEST_ROOM)):
+        for x in range(0, len(TEST_ROOM[0])):
+            xo = x*tz
+            yo = y*tz
+            if TEST_ROOM[y][x] == 1:
+                _color = COLOR["WHITE"]
+            else:
+                _color = COLOR["BLACK"]
+            pygame.draw.rect(surface=surface, color=_color,
+                             rect=pygame.Rect(xo, yo, tz-1, tz-1))
+
+
+def draw_player():
+        # Draw Player as a square
     pygame.draw.rect(surface=surface, color=COLOR["YELLOW"],
                      rect=pygame.Rect(player.px-player.size/2,
                                       player.py-player.size/2,
                                       player.size, player.size))
+    pygame.draw.line(surface=surface,color=COLOR["YELLOW"],
+                        start_pos=(player.px,player.py),
+                        end_pos=(player.px+math.cos(player.pa)*player.size*2,
+                                 player.py+math.sin(player.pa)*player.size*2),width=int(player.size/4))
+
+
+def draw():
+    # draw background
+    surface.fill(COLOR["DARK_GRAY"])
+    draw_2d_map()
+    draw_player()
+
 
 
 def handle_events():
@@ -53,32 +76,38 @@ def handle_events():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
-                player.velocity[1] = -1
-
-            if event.key == pygame.K_a:
-                player.velocity[0] = -1
+                player.movement=1
 
             if event.key == pygame.K_s:
-                player.velocity[1] = 1
+                player.movement=-1
+                
+            if event.key == pygame.K_a:
+                player.da-=3
 
             if event.key == pygame.K_d:
-                player.velocity[0] = 1
-
+                player.da+=3
+                
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_w or event.key == pygame.K_s:
-                player.velocity[1] = 0
-
-            if event.key == pygame.K_a or event.key == pygame.K_d:
-                player.velocity[0] = 0    
+            if event.key==pygame.K_w or  event.key==pygame.K_s:
+                player.movement=0
+            
+            if event.key==pygame.K_a or event.key==pygame.K_d:
+                player.da=0
+            
 
 
 clock = pygame.time.Clock()
-surface = pygame.display.set_mode(SETTINGS["WINDOW_SIZE"])
+surface = pygame.display.set_mode(SETTINGS["WINDOW_SIZE"],pygame.FULLSCREEN)
 surface.fill(COLOR["DARK_GRAY"])
 px, py = SETTINGS["PLAYER_SPOS"]
-player = Player(px, py, SETTINGS["PLAYER_SIZE"], SETTINGS["PLAYER_SPEED"])
+player = Player(px, py, SETTINGS["PLAYER_SIZE"],
+                        SETTINGS["PLAYER_SPEED"],
+                        SETTINGS["PLAYER_SANGLE"],
+                        SETTINGS["PLAYER_TSPEED"])
+
 
 def main():
 
@@ -87,12 +116,11 @@ def main():
     pygame.display.set_icon(pygame.image.load(SETTINGS["ICON_URL"]))
 
     while True:
-        dt=clock.tick(SETTINGS["FPS"]) / 1000
+        dt = clock.tick(SETTINGS["FPS"]) / 1000
         handle_events()
         draw()
         update(dt)
         pygame.display.update()
-
 
 
 if __name__ == "__main__":
--- settings.py
+++ settings.py
@@ -1,11 +1,13 @@
 SETTINGS={
     "WINDOW_SIZE":(1280,720),
-    "FPS":60,
+    "FPS":15,
     "TITLE":"Tomb of The Unknown God",
     "ICON_URL":"logo.png",
     "PLAYER_SPOS":(170,170),
-    "PLAYER_SIZE":8,
+    "PLAYER_SIZE":16,
     "PLAYER_SPEED":60,
+    "PLAYER_SANGLE":0,
+    "PLAYER_TSPEED":0.5,
     "TILE_SIZE":32
 }
 
